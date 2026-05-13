d4244304ed302c682b3ea28236b8714cdd90328e
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-13 11:49:52 +0700

    fix: fix api response for get customer by account id

--- cart-service/src/main/java/iuh/fit/cartservice/client/UserServiceClient.java
+++ cart-service/src/main/java/iuh/fit/cartservice/client/UserServiceClient.java
@@ -1,6 +1,8 @@
 package iuh.fit.cartservice.client;
 
 import iuh.fit.cartservice.dto.CustomerResponse;
+import iuh.fit.shared.api.ApiResponse;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
 
-    @GetMapping("/api/v1/customers/account/{accountId}")
-    CustomerResponse getCustomerByAccountId(@PathVariable("accountId") String accountId);
+    @GetMapping("/api/v1/user/customers/account/{accountId}")
+    ApiResponse<CustomerResponse> getCustomerByAccountId(@PathVariable("accountId") String accountId);
+
 }
